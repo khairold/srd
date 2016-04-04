29a388c9361fa5412e0d152cad38af873d5a4dc2
khairold <khairold@gmail.com>
2016-04-04 19:36:35 +0800

    adding redux

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+
+const Root = () => <div>lla</div>
+
+render(<Root />, document.getElementById('root'))
