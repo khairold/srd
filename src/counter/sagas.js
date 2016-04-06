11fc58958e9dc2fcb098d559e40df55971739891
Khairold Safri Ibrahim <khairold@gmail.com>
2016-04-06 22:05:16 +0800

    firebase yield

--- src/counter/sagas.js
+++ src/counter/sagas.js
@@ -4,19 +4,30 @@ import { put, call } from 'redux-saga/effects'
 import Firebase from 'firebase'
 const ref = new Firebase('https://srd.firebaseio.com/')
 
-ref.child('projects').once('value').then(function(snapshot) {
+
+
+function callfb() {
+	return ref.child('projects').once('value').then(function(snapshot) {
   // renderBlog(snapshot.val())
-  console.log(snapshot.val())
-}, function(error) {
-  console.error(error)
-});
+	  // console.log(snapshot.val())
+	  return snapshot.val()
+	}, function(error) {
+	  console.error(error)
+	})
+}
 
 export function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+// export function* incrementAsync() {
+//   yield call(delay, 1000)
+//   yield put({type: 'INCREMENT_COUNTER'})
+// }
+
 export function* incrementAsync() {
-  yield call(delay, 1000)
+  const data = yield call(callfb)
+  console.log(data)
   yield put({type: 'INCREMENT_COUNTER'})
 }
 
