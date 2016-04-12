2940e6c4b42fb8d0867f12b3936ef960217ce815
Khairold Safri Ibrahim <khairold@gmail.com>
2016-04-13 00:10:08 +0800

    marked

--- src/redux/project/containerSingle.js
+++ src/redux/project/containerSingle.js
@@ -7,16 +7,28 @@ import _ from 'lodash'
 
 import { Link } from 'react-router'
 
+import marked from 'marked'
+
+const Marked = ({ markedText }) => <div dangerouslySetInnerHTML={{ __html: marked(markedText, { sanitize: true }) }} />
+
 class ProjectSingle extends Component {
   render() {
     const { project } = this.props
-    const projectName = project && project.name ? <h1>{project.name}</h1> : null
+
+    const ProjectName = project && project.name ? <h1>{project.name}</h1> : null
+    const Description = project && project.description ? <Marked markedText={project.description} /> : null
+    const Updates = project && project.updates ? <Marked markedText={project.updates} /> : null
+
     return (
       <div>
         <div>
           <Link to={`/projects`}>Project List</Link>
         </div>
-        {projectName}
+        {ProjectName}
+        <div>
+          {Description}
+          {Updates}
+        </div>
       </div>
     )
   }
